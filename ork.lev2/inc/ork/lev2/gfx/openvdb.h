bc24b46e4c983fa68b975cb4a4f03179a41a1199
RedHawk989 <48768484+RedHawk989@users.noreply.github.com>
2026-06-23 14:44:58 -0700

    lev2/openvdb: use the version-agnostic openvdb::tree namespace
    openvdb.h hard-coded openvdb::v13_0::tree::InternalNode, which fails to compile
    against any other installed OpenVDB ABI version (e.g. v12_0):
    
      error: no member named 'v13_0' in namespace 'openvdb'; did you mean 'v12_0'?
    
    OpenVDB exposes its versioned namespace as an inline namespace inside openvdb,
    so openvdb::tree::InternalNode resolves to whichever version is installed. This
    matches how the adjacent Tree4 / Grid typedefs in this same file are already
    written (openvdb::tree::Tree4, openvdb::Grid).

--- ork.lev2/inc/ork/lev2/gfx/openvdb.h
+++ ork.lev2/inc/ork/lev2/gfx/openvdb.h
@@ -112,8 +112,8 @@ using vdb_grid_test = openvdb::Grid<vdb_tree_test>;
 using vdb_grid_test_ptr_t  = std::shared_ptr<vdb_grid_test>;
 
 using vdb_tree_test_leaf_t = vdb_tree_test::LeafNodeType;                                        // L3 (   8^3 [512] voxels )
-using vdb_tree_test_int2_t = openvdb::v13_0::tree::InternalNode<vdb_tree_test_leaf_t,L2_SIZE>;   // L2 ( 128^3 [2M]  voxels )
-using vdb_tree_test_int1_t = openvdb::v13_0::tree::InternalNode<vdb_tree_test_int2_t,L1_SIZE>;   // L1 (4096^3 [64G] voxels )
+using vdb_tree_test_int2_t = openvdb::tree::InternalNode<vdb_tree_test_leaf_t,L2_SIZE>;   // L2 ( 128^3 [2M]  voxels )
+using vdb_tree_test_int1_t = openvdb::tree::InternalNode<vdb_tree_test_int2_t,L1_SIZE>;   // L1 (4096^3 [64G] voxels )
 using vdb_tree_test_root_t = vdb_tree_test::RootNodeType;                                        // L0 ??? voxels
 
 ///////////////////////////////////////////////////////////////////////////////
